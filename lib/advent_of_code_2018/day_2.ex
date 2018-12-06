75a4a01f5256cec3263495153ba9ef2334eb81f0
Johan Tell <johan@mintcore.se>
2018-12-06 21:00:50 +0100

    Finished day2 part2. It's not pretty though

--- lib/advent_of_code_2018/day_2.ex
+++ lib/advent_of_code_2018/day_2.ex
@@ -8,6 +8,62 @@ defmodule AdventOfCode2018.Day2 do
     doubles * triples
   end
 
+  def similarities_of_best_match(input) do
+    {string1, string2, _score} = find_two_similar_ids(input)
+
+    List.myers_difference(string1, string2)
+    |> Enum.reduce([], fn
+      {:eq, list}, acc -> acc ++ list
+      _, acc -> acc
+    end)
+    |> to_string
+  end
+
+  def find_two_similar_ids(input) do
+    box_ids =
+      parse_box_ids(input)
+      |> Enum.map(&String.to_charlist/1)
+
+    match_ids(box_ids)
+  end
+
+  def match_ids([h | t]), do: match_ids([h], {nil, nil, 0}, t)
+
+  defp match_ids(matched_ids, {_box_id1, _box_id2, best_score} = best_match, [box_id | rest]) do
+    matched_ids
+    |> Enum.reduce({nil, 0}, fn string, acc ->
+      case calculate_match_score(string, box_id) do
+        score when score > best_score ->
+          {string, score}
+
+        _ ->
+          acc
+      end
+    end)
+    |> case do
+      {string, score} when score > best_score ->
+        match_ids(matched_ids ++ [box_id], {string, box_id, score}, rest)
+
+      _ ->
+        match_ids(matched_ids ++ [box_id], best_match, rest)
+    end
+  end
+
+  defp match_ids(_, best_match, _), do: best_match
+
+  def calculate_match_score(charlist, charlist2) do
+    calculate_match_score([], charlist, charlist2)
+    |> Enum.sum()
+  end
+
+  defp calculate_match_score(score, [head1 | rest1], [head2 | rest2]) do
+    result = if head1 == head2, do: 1, else: 0
+
+    calculate_match_score(score ++ [result], rest1, rest2)
+  end
+
+  defp calculate_match_score(score, _, _), do: score
+
   defp increase_doubles_and_triples(box_id, {total_doubles, total_triples}) do
     {doubles, triples} =
       box_id
--- test/advent_of_code_2018/day_2_test.exs
+++ test/advent_of_code_2018/day_2_test.exs
@@ -20,4 +20,49 @@ defmodule AdventOfCode2018.Day2Test do
       assert 5478 = Day2.create_checksum(@real_input)
     end
   end
+
+  describe "calculate_match_score" do
+    test "returns a score based on the similarities of the different lists" do
+      input1 = ~w(a b c d)
+      input2 = ~w(a b c d)
+
+      assert 4 = Day2.calculate_match_score(input1, input2)
+    end
+  end
+
+  describe "match_ids" do
+    test "returns the best matching ids" do
+      list1 = ~w(a b c d e f g)
+      list2 = ~w(a b c d e g g)
+
+      list_of_ids = [
+        list1,
+        ~w(a b n u e f g),
+        ~w(a b l d j f g),
+        list2
+      ]
+
+      assert {^list1, ^list2, 6} = Day2.match_ids(list_of_ids)
+    end
+  end
+
+  describe "find_two_similar_ids" do
+    test "returns the two most similar ids" do
+      input = ~s(abcd ahdu aoeu mduq sjqs abvd iuqo)
+
+      assert {'abcd', 'abvd', 3} = Day2.find_two_similar_ids(input)
+    end
+  end
+
+  describe "similarities_of_best_match" do
+    test "returns similarities of the two  most similar ids" do
+      input = ~s(abcd ahdu aoeu mduq sjqs abvd iuqo)
+
+      assert "abd" = Day2.similarities_of_best_match(input)
+    end
+
+    test "returns similarities of the two  most similar ids when passed the real data" do
+      assert "qyzphxoiseldjrntfygvdmanu" = Day2.similarities_of_best_match(@real_input)
+    end
+  end
 end
