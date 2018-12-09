315337d9ed9293501b1ba69a0872339ffc98472e
Johan Tell <johan@mintcore.se>
2018-12-09 14:12:01 +0100

    Finish day3 part 2

--- lib/advent_of_code_2018/day_3.ex
+++ lib/advent_of_code_2018/day_3.ex
@@ -1,28 +1,48 @@
 defmodule Day3 do
   def squares_claimed_by_many(claims) do
-    {_, overlapping} = claims
-    |> Enum.map(&parse_claim/1)
-    |> Enum.map(&define_area/1)
-    |> Enum.flat_map(&expand_coordinates/1)
-    |> Enum.reduce({MapSet.new(), MapSet.new()}, fn coordinate, {processed, overlaying} ->
-      case MapSet.member?(processed, coordinate) do
-        true -> {processed, MapSet.put(overlaying, coordinate)}
-        false -> {MapSet.put(processed, coordinate), overlaying}
-      end
-    end)
+    claims
+    |> parse_claims()
+    |> Enum.reduce(%{}, &collect_overlapping/2)
+    |> Enum.reduce(0, fn
+      {_, [_, _ | _]}, acc ->
+        acc + 1
 
-    Enum.count(overlapping)
+      _, acc ->
+        acc
+    end)
   end
 
-  defp expand_coordinates(%{x: x_range, y: y_range}) do
-    Enum.flat_map(x_range, fn x ->
-      Enum.reduce(y_range, [], fn y, acc ->
-        acc ++ [{x, y}]
+  def non_overlapping_id(claims) do
+    claims = parse_claims(claims)
+    all_ids = Enum.reduce(claims, [], fn %{id: id}, acc -> [id | acc] end)
+    overlappings = Enum.reduce(claims, %{}, &collect_overlapping/2)
+
+    [id] = Enum.reduce(overlappings, all_ids, fn
+      {_, [_, _ | _] = claim_ids}, acc ->
+        acc -- claim_ids
+
+      _, acc ->
+        acc
+    end)
+
+    id
+  end
+
+  defp parse_claims(claims) do
+    claims
+    |> Enum.map(&parse_claim/1)
+    |> Enum.map(&define_area/1)
+  end
+
+  defp collect_overlapping(%{id: id, x: x_range, y: y_range}, acc) do
+    Enum.reduce(x_range, acc, fn x, x_acc ->
+      Enum.reduce(y_range, x_acc, fn y, y_acc ->
+        Map.update(y_acc, {x, y}, [id], &(&1 ++ [id]))
       end)
     end)
   end
 
-  defp define_area(%{id: id, x: x, y: y, area_x: ax, area_y: ay}) do
+  defp define_area(%{id: id, x: x, y: y, width: ax, height: ay}) do
     %{
       id: id,
       x: (x + 1)..(x + ax),
@@ -30,7 +50,7 @@ defmodule Day3 do
     }
   end
 
-  @claim_regex ~r/#(?<id>\d+) @ (?<x>\d+),(?<y>\d+): (?<area_x>\d+)x(?<area_y>\d+)/
+  @claim_regex ~r/#(?<id>\d+) @ (?<x>\d+),(?<y>\d+): (?<width>\d+)x(?<height>\d+)/
   def parse_claim(claim) do
     Regex.named_captures(@claim_regex, claim)
     |> Enum.map(fn {key, value} -> {String.to_atom(key), String.to_integer(value)} end)
--- test/advent_of_code_2018/day_3_test.exs
+++ test/advent_of_code_2018/day_3_test.exs
@@ -21,11 +21,27 @@ defmodule Day3Test do
     end
   end
 
+  describe "non_overlapping_id/1" do
+    test "returns 3 when passed the example input" do
+      claims = [
+        "#1 @ 1,3: 4x4",
+        "#2 @ 3,1: 4x4",
+        "#3 @ 5,5: 2x2"
+      ]
+
+      assert 3 = Day3.non_overlapping_id(claims)
+    end
+
+    test "returns 742 when passed real input" do
+      assert 742 = Day3.non_overlapping_id(@real_input)
+    end
+  end
+
   describe "parse_claim/1" do
     test "parses a claim" do
       claim = "#1 @ 1,3: 4x5"
 
-      assert %{id: 1, x: 1, y: 3, area_x: 4, area_y: 5} = Day3.parse_claim(claim)
+      assert %{id: 1, x: 1, y: 3, width: 4, height: 5} = Day3.parse_claim(claim)
     end
   end
 end
